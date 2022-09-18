77193f46bf21bd3a5c41adda80344ae22ba9fe46
eu <diego_thegarden@hotmail.com>
2022-09-18 19:37:58 +0200

    create orders

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require("./models/product.js");
 const User = require("./models/user.js");
 const Cart = require("./models/cart.js");
 const CartItem = require("./models/cart-item.js");
+const Order = require("./models/order.js");
+const OrderItem = require("./models/order-item.js");
 
 const app = express();
 
@@ -17,7 +19,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { serialize } = require("v8");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -48,6 +49,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 //sincroniza os models coa db
 //elimnar force: true en deploy xa que forza overwrite
